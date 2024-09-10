1d34271ca52f197a5db00b5a0e8f05c6dde15521
Murtaza Vohra <murtaza.vohra@intelliticks.com>
2024-09-10 16:58:13 +0530

    Refactor Dashboard component to set initial loading state to true and display a full page loader while data is being fetched

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -6,20 +6,28 @@ import Header from "./Header";
 import { getDataRequestsApi } from "@/services/api-services";
 import { useEffect, useState } from "react";
 import ProtectedComponent from "@/components/custom/ProtectedComponent";
+import FullPageLoader from "@/components/custom/FullPageLoader";
 
 const Dashboard: React.FC = () => {
     const [data, setData] = useState<DataRequest[]>([]);
+    const [loading, setLoading] = useState(true);
     const getData = async () => {
         try {
             const data = await getDataRequestsApi();
             setData(data.dataRequests);
         } catch (error) {
             console.error(error);
+        } finally {
+            setLoading(false);
         }
     };
     useEffect(() => {
         getData();
     }, []);
+
+    if (loading) {
+        return <FullPageLoader/>;
+    }
 
     return (
         <div className="flex min-h-screen w-screen flex-col overflow-x-hidden">
